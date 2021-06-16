5506cdddb5d7985282e4dfc9f79e1102bbf73f9c
Jostin Gudiño <74581107+saquiador@users.noreply.github.com>
2021-06-16 05:11:37 +0000

    bot de ayuda

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./comandos').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./comandos/${file}`);
-    client.commands.set(command.name, command);
+  const command = require(`./comandos/${file}`);
+  client.commands.set(command.name, command);
 }
 
 
@@ -38,44 +38,44 @@ client.giveawaysManager = new GiveawaysManager(client, {
 const estados = ['RBXS Land', 'legit', 'r!help', 'Jugando roblox']
 
 client.on('ready', () => {
-  
+
   setInterval(() => {
     function presence() {
       client.user.setPresence({
-        status: 'dnd', 
+        status: 'dnd',
         activity: {
-         name: estados[Math.floor(Math.random() * estados.length)],
-        type: 'PLAYING',
-      }
+          name: estados[Math.floor(Math.random() * estados.length)],
+          type: 'PLAYING',
+        }
       })
     }
     presence()
   }, 10000);
-  
+
   console.log('go')
 });
 
 
-client.on('message', (message) => { 
-  
-  
+client.on('message', (message) => {
+
+
   //Abrimos un evento message, esto es muy importante porque es donde estarán los comandos
 
 
 
-if(message.author.bot) return; //Con esto hacemos que el bot no responda a mensajes de otros bots lo cual evitará que entre en bucles
-if(!message.content.startsWith(prefix)) return; //Aquí hacemos que si el mensaje no empieza con el prefix el bot no responda
+  if (message.author.bot) return; //Con esto hacemos que el bot no responda a mensajes de otros bots lo cual evitará que entre en bucles
+  if (!message.content.startsWith(prefix)) return; //Aquí hacemos que si el mensaje no empieza con el prefix el bot no responda
 
-let usuario = message.mentions.members.first() || message.member; //Definimos el usuario
-const args = message.content.slice(prefix.length).trim().split(/ +/g); //Definimos los argumentos
-const command = args.shift().toLowerCase(); //Definimos el comandos
+  let usuario = message.mentions.members.first() || message.member; //Definimos el usuario
+  const args = message.content.slice(prefix.length).trim().split(/ +/g); //Definimos los argumentos
+  const command = args.shift().toLowerCase(); //Definimos el comandos
   let cmd = client.commands.find((c) => c.name === command || c.alias && c.alias.includes(command));
-if(cmd){
-cmd.execute(client, message, args)
+  if (cmd) {
+    cmd.execute(client, message, args)
 
-}
+  }
 
-//Aquí irían los comandos que pondremos más adelante
+  //Aquí irían los comandos que pondremos más adelante
 
 });
 
