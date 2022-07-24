b90d4f2d3ecb75e8008a20d9e6ef9ec49310a6ab
Jostin Gudiño <74581107+saquiador@users.noreply.github.com>
2022-07-24 23:38:41 +0000

    actual

--- comandos/avatar.js
+++ comandos/avatar.js
@@ -10,11 +10,12 @@ execute (client, message, args){
 
 let usuario = message.mentions.members.first() || message.member
 
-let embedavatar = new Discord.MessageEmbed()
+const embedavatar = new Discord.MessageEmbed()
 
 .setTitle(`Avatar de **${usuario.user.username}**`)
 .setImage(usuario.user.displayAvatarURL({ size: 1024, dynamic: true}))
 .setFooter(`Pedido Por ${message.member.displayName}`)
+.setColor('#c50000')
 
 message.channel.send(embedavatar)
 
